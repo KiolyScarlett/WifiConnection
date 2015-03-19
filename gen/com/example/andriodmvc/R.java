d04fcd11dbdbfbe117628d9d45945ebe1feee0fc
Scarlett <Scarlett@Scarlett-PC>
2015-03-19 13:40:31 +0800

    addbutton

--- gen/com/example/andriodmvc/R.java
+++ gen/com/example/andriodmvc/R.java
@@ -24,21 +24,24 @@ public final class R {
         public static final int background1=0x7f020000;
         public static final int button1=0x7f020001;
         public static final int buttons=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int icon=0x7f020004;
-        public static final int incoming=0x7f020005;
-        public static final int outgoing=0x7f020006;
-        public static final int retouxiang=0x7f020007;
-        public static final int searchbg1=0x7f020008;
-        public static final int searchbg2=0x7f020009;
+        public static final int enter=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int icon=0x7f020005;
+        public static final int incoming=0x7f020006;
+        public static final int outgoing=0x7f020007;
+        public static final int plus=0x7f020008;
+        public static final int retouxiang=0x7f020009;
+        public static final int searchbg1=0x7f02000a;
+        public static final int searchbg2=0x7f02000b;
     }
     public static final class id {
         public static final int MessageBox=0x7f080000;
-        public static final int MessageButton=0x7f08000a;
+        public static final int MessageButton=0x7f08000b;
         public static final int MessageText=0x7f080009;
-        public static final int action_settings=0x7f08000c;
+        public static final int PlusButton=0x7f08000a;
+        public static final int action_settings=0x7f08000d;
         public static final int device_list=0x7f080003;
-        public static final int list=0x7f08000b;
+        public static final int list=0x7f08000c;
         public static final int messagedetail_row_date=0x7f080006;
         public static final int messagedetail_row_name=0x7f080005;
         public static final int messagedetail_row_text=0x7f080007;
